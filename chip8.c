6ac60eb29c3c542314529d82cc7339d34907e45c
Mustafa <senioramin06@gmail.com>
2026-03-10 16:02:08 -0400

    added a few more instructions and cleaned up the cases

--- chip8.c
+++ chip8.c
@@ -35,140 +35,151 @@ uint8_t fontset[80] = {
 };
 
 void powerOn(chip8 *cpu){
-    // clears mem
-    for(int i = 0; i < 4096; i++){
-        cpu->memory[i] = 0;
+    void powerOn(chip8 *cpu){
+        // clears mem
+        for(int i = 0; i < 4096; i++){
+            cpu->memory[i] = 0;
+        }
+        // clears display
+        for(int i = 0; i < 2048; i++) {
+            cpu->display[i] = 0;
+        }
+        // clears both stacks
+        for(int i = 0; i < 16; i++){
+            cpu->V[i] = 0;
+            cpu->stack[i] = 0;
+        }
+        cpu->I = 0;
+        // mem and intructions usually start at 0x200, hence why we positioned it here
+        cpu->pc = 0x200;
+        cpu->sp = 0;
+        cpu->delaytimer = 0;
+        cpu->soundtimer = 0;
+        // load fonts into mem starting from 0x50
+        for(int i = 0; i < 80; i++){
+            cpu->memory[0x50 + i] = fontset[i];
+        }
     }
-    // clears display
-    for(int i = 0; i < 2048; i++) {
-        cpu->display[i] = 0;
+
+    void loadROM(chip8 *cpu, char *filename){
+        FILE *file;
+        file = fopen(filename, "rb");
+        if (file == NULL){
+            perror("Error opening file");
+            return;
+        }
+        fread((void *)&cpu->memory[0x200], 1, 3584, file);
+
+        for(int i = 0; i < 10; i++) {
+            printf("Byte at 0x%03X: %02X\n", 0x200 + i, cpu->memory[0x200 + i]);
+        }
+        fclose(file);
     }
-    // clears both stacks
-    for(int i = 0; i < 16; i++){
-        cpu->V[i] = 0;
-        cpu->stack[i] = 0;
-    }
-    cpu->I = 0;
-    // mem and intructions usually start at 0x200, hence why we positioned it here
-    cpu->pc = 0x200;
-    cpu->sp = 0;
-    cpu->delaytimer = 0;
-    cpu->soundtimer = 0;
-    // load fonts into mem starting from 0x50
-    for(int i = 0; i < 80; i++){
-        cpu->memory[0x50 + i] = fontset[i];
-    }
-}
 
-void loadROM(chip8 *cpu, char *filename){
-    FILE *file;
-    file = fopen(filename, "rb");
-    if (file == NULL){
-        perror("Error opening file");
-        return;
-    }
-    fread((void *)&cpu->memory[0x200], 1, 3584, file);
+    void cycle(chip8 *cpu){
+        // Out of bounds
+        if(cpu->pc > 4094){
+            fprintf(stderr, "Program counter out of bounds: 0x%03X\n", cpu->pc);
+            return;
+        }
+        // FETCH
+        uint16_t opcode = (cpu->memory[cpu->pc] << 8) | cpu->memory[cpu->pc + 1];
 
-    for(int i = 0; i < 10; i++) {
-    printf("Byte at 0x%03X: %02X\n", 0x200 + i, cpu->memory[0x200 + i]);
-    }
-    fclose(file);  
-}
+        // DECODE
+        uint8_t T = (opcode & 0xF000) >> 12;
+        uint8_t X = (opcode & 0x0F00) >> 8;
+        uint8_t Y = (opcode & 0x00F0) >> 4;
+        uint8_t N = (opcode & 0x000F);
+        uint8_t NN = (opcode & 0x00FF);
+        uint16_t NNN = (opcode & 0x0FFF);
 
-void cycle(chip8 *cpu){
-    // Out of bounds
-    if(cpu->pc > 4094){
-        fprintf(stderr, "Program counter out of bounds: 0x%03X\n", cpu->pc);
-        return;
-    }
-    // FETCH
-    uint16_t opcode = (cpu->memory[cpu->pc] << 8) | cpu->memory[cpu->pc + 1];
-
-    // DECODE
-    uint8_t T = (opcode & 0xF000) >> 12;
-    uint8_t X = (opcode & 0x0F00) >> 8;
-    uint8_t Y = (opcode & 0x00F0) >> 4;
-    uint8_t N = (opcode & 0x000F);
-    uint8_t NN = (opcode & 0x00FF);
-    uint16_t NNN = (opcode & 0x0FFF);
-
-    // EXECUTE
-    switch(T) {
-        case 0x6: // Instruction 6XNN: Set register VX to NN
-            cpu->V[X] = NN;
-            cpu->pc += 2;
-            break;
-
-        case 0xA: // Instruction ANNN: Set index register I to NNN
-            cpu->I = NNN;
-            cpu->pc += 2;
-            break;
-
-        case 0x7:
-            cpu->V[X] += NN;
-            cpu->pc += 2;
-            break;
-
-        case 0x3:
-            if (cpu->V[X] == NN){
-                cpu->pc += 4;
-            }   else {
-                cpu->pc += 2;
-            }
-            break;
-
-        case 0x4:
-            if (cpu->V[X] != NN){
-                cpu->pc += 4;
-            } else {
-                cpu->pc += 2;
-            }
-            break;
-    
-        case 0x0:
-            if (opcode == 0x00E0){
-                for(int i = 0; i < 2048; i++){
-                    cpu->display[i] = 0;
+        // EXECUTE
+        switch(T) {
+            case 0x0:  // Intruction 0NNN: Reset display
+                if (opcode == 0x00E0){
+                    for(int i = 0; i < 2048; i++){
+                        cpu->display[i] = 0;
+                    }
+                    cpu->pc += 2;
                 }
-            }
-            break;
-        
-        
-        
+                else if (opcode == 0x00EE){ // Subroutine anihalator
+                    cpu->sp --;
+                    cpu->pc = cpu->stack[cpu->sp];
+                    cpu->pc += 2;
+                }
+                break;
 
-        
+            case 0x1:
+                cpu->pc = NNN;
+                break;
 
+            case 0x2: // Instruction 2NNN: Subroutine call
+                cpu->stack[cpu->sp] = cpu->pc;
+                cpu->sp ++;
+                cpu->pc = NNN;
+                break;
 
+            case 0x3:  // Intruction 3NNN: Skip if equal
+                if (cpu->V[X] == NN){
+                    cpu->pc += 4;
+                }   else {
+                    cpu->pc += 2;
+                }
+                break;
 
+            case 0x4:  // Intruction 4NNN: Skip if not equal
+                if (cpu->V[X] != NN){
+                    cpu->pc += 4;
+                } else {
+                    cpu->pc += 2;
+                }
+                break;
 
+            case 0x5:
+                if (cpu->V[X] == cpu->V[Y]){
+                    cpu->pc += 4;
+                } else {
+                    cpu->pc += 2;
+                }
+                break;
 
+            case 0x6: // Instruction 6XNN: Set register VX to NN
+                cpu->V[X] = NN;
+                cpu->pc += 2;
+                break;
 
+            case 0x7:
+                cpu->V[X] += NN;
+                cpu->pc += 2;
+                break;
 
+            case 0xA: // Instruction ANNN: Set index register I to NNN
+                cpu->I = NNN;
+                cpu->pc += 2;
+                break;
 
+            case 0xF:
+                switch(NN) {
+                    case 0x1E:  // Intruction FX
+                        cpu->I += cpu->V[X];
+                        cpu->pc += 2;
+                        break;
+                }
+                break;
 
-
-
-
-
-
-
-
-
-
-
-        default:
-            printf("Opcode not implemented yet: %04X\n", opcode);
-            cpu->pc += 2;
-            break;
+            default:
+                printf("Opcode not implemented yet: %04X\n", opcode);
+                cpu->pc += 2;
+                break;
+        }
     }
 
-}
 
-
-int main(){
-    chip8 my_cpu;
-    powerOn(&my_cpu);
-    loadROM(&my_cpu, "Pong (alt).ch8");
-    cycle(&my_cpu);
-    return 0;
+    int main(){
+        chip8 my_cpu;
+        powerOn(&my_cpu);
+        loadROM(&my_cpu, "Pong (alt).ch8");
+        cycle(&my_cpu);
+        return 0;
+    }
 }
